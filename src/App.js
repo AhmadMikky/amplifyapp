ed00faf41d864d18fc4f451c9579625d0dc669af
AhmadMikky <amm00081@mix.wvu.edu>
2023-11-02 19:52:48 -0400

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <header className="App">
+    <div className="App">
+     
         <h1>CS230L</h1>   
         <h2>Section-03</h2>  
         <p>WVU ID 800378834</p>  
         <p>Hi, my name is Ahmad Mikky</p>  
        
-      </header>
+     
     </div>
   );
 }
