4c76db8256e4bb88b62739e63873dfad44838399
AhmadMikky <amm00081@mix.wvu.edu>
2023-11-02 19:37:38 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CS230L</h1>   
+        <h2>Section-03</h2>  
+        <p>WVU ID 800378834</p>  
+        <p>Hi, my name is Ahmad Mikky</p>  
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
